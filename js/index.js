9364ce4121639f4edd4bbd3f6cdfd902f3c5fe20
Valer11 <Valer11@users.noreply.github.com>
2021-09-22 15:14:18 -0600

    Done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
+//updateInnerHTML() function
+
 function updateInnerHTML(selector, newValue) {
     document.querySelector(selector).innerHTML = newValue;
 }
 
-updateInnerHTML('h2.intro', 'Something New');
+//Testing
+//updateInnerHTML('h2.intro', 'Something New');
+
+// 1. 
+
+function strong(value) {
+    return '<strong>' + value + '</strong>';
+   }
+
+
+//Testing
+//strong('hello');
+
+
+// 2.
+
+var importantParagraph = document.querySelector('p.important').innerHTML;
+
+// 3.
+
+importantParagraph = strong(importantParagraph);
+
+// 4. 
+updateInnerHTML('p.important', importantParagraph);
